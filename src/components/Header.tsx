ae1d874095eb7dc6d987af2e1b55c4da05770c64
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-09-21 10:52:40 +0300

    Make title in `Header` a link to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="py-4">
       <div className="max-w-screen-2xl mx-auto w-full px-2">
-        <h1 className="text-3xl font-bold text-neutral-300">hooks</h1>
+        <Link href="/" className="hover:underline">
+          <h1 className="text-3xl font-bold text-neutral-300 w-fit">hooks</h1>
+        </Link>
       </div>
     </header>
   );
